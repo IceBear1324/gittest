dfa8f65db7202e2ac50389932f1300ef7ee36901
cui <563575629@qq.com>
2019-10-29 22:54:27 +0800

    初始化提交

---
+++ src/main/java/com/bjyy/simplecase/CyszSimplecase1Application.java
@@ -0,0 +1,13 @@
+package com.bjyy.simplecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyszSimplecase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyszSimplecase1Application.class, args);
+	}
+
+}
